93a83c9a45d6ef3f781bcd8d2b1ad1873776c60b
lpruuli <166364513+lpruuli@users.noreply.github.com>
2025-05-07 06:40:12 +0000

    Fix wattage calculation and redraw issue

--- ryhm1/takisti1.js
+++ ryhm1/takisti1.js
@@ -7,11 +7,13 @@ var Resistor = /** @class */ (function () {
         this.y = y;
         this.height = 40;
         this.u = 0;
+        this.i = 0;
+        this.w = 0;
         this.width = this.endx - this.startx;
         this.draw();
     }
     Resistor.prototype.draw = function () {
-        this.g.clearRect(this.startx, this.y - this.height / 2, this.width, this.height);
+        this.g.clearRect(this.startx - 10, this.y - this.height / 2 - 10, this.width + 20, this.height + 20);
         this.g.beginPath();
         this.g.moveTo(this.startx, this.y);
         this.g.lineTo(this.startx + this.width / 4, this.y);
@@ -20,14 +22,35 @@ var Resistor = /** @class */ (function () {
         this.g.lineTo(this.endx, this.y);
         this.g.stroke();
         this.g.fillText(this.r + " Ω", this.startx + this.width / 3, this.y + 3);
-        this.g.fillText(this.u + " V", this.startx + this.width / 4, this.y - this.height / 3 - 2);
+        this.g.fillText(this.u + " V, " + this.i.toFixed(3) + " A", this.startx + this.width / 4, this.y - this.height / 3 - 2);
+        this.g.fillText(this.w + " W", this.startx + this.width + 10, this.y + 6);
     };
     Resistor.prototype.setR = function (r) {
         this.r = r;
+        this.calculateCurrent();
         this.draw();
     };
     Resistor.prototype.getR = function () {
         return this.r;
+    };
+    Resistor.prototype.setU = function (u) {
+        this.u = u;
+        this.calculateCurrent();
+        this.calculateW();
+        this.draw();
+    };
+    Resistor.prototype.getU = function () {
+        return this.u;
+    };
+    Resistor.prototype.calculateCurrent = function () {
+        this.i = this.u / this.r;
+    };
+    Resistor.prototype.calculateW = function () {
+        this.w = this.u * this.i;
+        this.draw();
+    };
+    Resistor.prototype.getW = function () {
+        return this.w;
     };
     return Resistor;
 }());
--- ryhm1/takisti1.ts
+++ ryhm1/takisti1.ts
@@ -2,6 +2,8 @@ class Resistor{
     protected width: number;
     protected height: number=40;
     protected u: number=0;
+    protected i: number=0;
+    protected w: number=0;
     constructor(protected r: number, protected g, 
          protected startx: number, protected endx: number, 
           protected y: number){
@@ -9,7 +11,7 @@ class Resistor{
         this.draw();
     }
     draw(){
-        this.g.clearRect(this.startx, this.y-this.height/2, this.width, this.height);
+        this.g.clearRect(this.startx, this.y - this.height / 2, this.width, this.height);
         this.g.beginPath();
         this.g.moveTo(this.startx, this.y);
         this.g.lineTo(this.startx+this.width/4, this.y);
@@ -18,13 +20,34 @@ class Resistor{
         this.g.lineTo(this.endx, this.y);
         this.g.stroke();
         this.g.fillText(this.r+" Ω", this.startx+this.width/3, this.y+3);
-        this.g.fillText(this.u+" V", this.startx+this.width/4, this.y-this.height/3-2);
+        this.g.fillText(this.u+" V, "+this.i.toFixed(3)+" A", this.startx+this.width/4, this.y-this.height/3-2);
+        this.g.fillText(this.w + " W", this.startx + this.width+10, this.y + 6);
     }
     setR(r:number){
         this.r=r;
+        this.calculateCurrent();
         this.draw();
     }
     getR():number{
         return this.r;
+    }
+    setU(u: number){
+        this.u=u;
+        this.calculateCurrent();
+        this.calculateW();
+        this.draw()
+    }
+    getU():number{
+        return this.u;
+    }
+    calculateCurrent(){
+        this.i=this.u/this.r;
+    }
+    calculateW(){
+        this.w = this.u * this.i;
+        this.draw();
+    }
+    getW():number{
+        return this.w;
     }
 }
